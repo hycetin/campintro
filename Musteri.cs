495b3dd1a5f09ff58012ff7c5537bfaa9e83b1ea
hycetin <77671735+hycetin@users.noreply.github.com>
2021-01-20 17:02:50 +0300

    Add files via upload

---
+++ Musteri.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classmetotDemo
+{
+    class Musteri
+    {
+        public string Adi { get; set; }
+        public string SoyAdi { get; set; }
+        public int Yasi { get; set; }
+        public string Cinsiyeti { get; set; }
+    }
+}
