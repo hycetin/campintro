b9c85f6c4e341d5aa800290774f0f557eb8adc28
hycetin <77671735+hycetin@users.noreply.github.com>
2021-01-20 17:03:21 +0300

    Add files via upload

---
+++ Musterimanager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classmetotDemo
+{
+    class Musterimanager
+    {
+
+
+
+
+
+
+        public void Ekle(Musteri kisiler) 
+        {
+            Console.WriteLine("Kayıt Başarılı" + "   " + "Hoşgeldiniz:" + " " +  kisiler.Adi + " " + kisiler.SoyAdi + " " + kisiler.Cinsiyeti);
+        
+        
+        
+        
+        
+        
+        }
+
+        public void Delete(Musteri kisiler) 
+        {
+            Console.WriteLine( "KAYDINIZ SİLİNMİŞTİR" + " " + kisiler.Adi + " " + kisiler.Cinsiyeti );
+        
+        
+        
+        }
+
+            
+        
+        
+        
+        
+        
+        
+         
+
+        
+        
+        
+        
+        
+        
+
+    }
+}
